43b862243b82a6e3d705bf208b8ef26b88d1f28d
frogbird <matthewparas2020@u.northwestern.edu>
2018-12-05 15:45:51 -0600

    unit test file

---
+++ setlist/main.py
@@ -0,0 +1,10 @@
+import unittest
+class PlaylistTest(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(True, True)
+    def test2(self):
+        self.assertEqual(True, True)
+    def test3(self):
+        self.assertFalse(True, False)
+
+if __name__== "__main__": unittest.main()
