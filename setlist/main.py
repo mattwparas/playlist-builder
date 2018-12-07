59a613d67a84aca5b2d69fdb410e9de1f6739889
frogbird <matthewparas2020@u.northwestern.edu>
2018-12-07 15:17:41 -0600

    adjusted objective functions

--- setlist/main.py
+++ setlist/main.py
@@ -1,28 +1,33 @@
-# import sys
-# sys.path.append('/Users/mwparas/Documents/PlaylistBuilder/setlist')
-
 import unittest
-from objective import evaluate_objective
+from objective import *
 import numpy as np
 import random
-# import importlib
+
 
 size = 10
-users = random.shuffle([x for x in range(1, size)])
-distance_mat = np.random.rand(size,size)
+users = [x for x in range(0, size)]
+distance_mat = np.random.randint(3, 45, size = (size,size))
 np.fill_diagonal(distance_mat, 0)
-
-print(distance_mat)
-
-# player_module = importlib.import_module(default_player_path)
-# Player = getattr(player_module, 'Player')
-
-# importlib.import_module(path_name)
 
 class PlaylistTest(unittest.TestCase):
     def test1(self):
+        '''
+        Generates a random distance matrix, performs the 2 opt swap
+        Used to test the functionality of the 2 opt
+        '''
+        size = 10
+        users = [x for x in range(0, size)]
+        distance_mat = np.random.randint(3, 45, size = (size,size))
+        np.fill_diagonal(distance_mat, 0)
+        perform_swaps(users, distance_mat)
+        print(evaluate_objective(users, distance_mat))
+        print(users)
         self.assertEqual(True, True)
     def test2(self):
+        '''
+        Generates a random distance matrix, performs the 2 opt swap
+        '''
+
         self.assertEqual(True, True)
     def test3(self):
         self.assertFalse(True == False)
--- setlist/objective.py
+++ setlist/objective.py
@@ -24,12 +24,12 @@ def evaluate_objective(tour, distance_matrix):
 
 
 
-def swaps(tour):
+def swaps(tour, distance_matrix):
     '''
     one iteration of a swap (2-opt)
     '''
     best_tour = tour[:]
-    best_objective = evaluate_objective(best_tour)
+    best_objective = evaluate_objective(best_tour, distance_matrix)
     tour_length = len(tour)
     
     for i in range(tour_length):
@@ -39,7 +39,7 @@ def swaps(tour):
             tour[i] = node2
             tour[j] = node1
             # Check objective value
-            test_objective = evaluate_objective(tour)
+            test_objective = evaluate_objective(tour, distance_matrix)
             # Compare objective values, update accordingly
             if test_objective < best_objective:
                 best_objective = test_objective
@@ -50,22 +50,32 @@ def swaps(tour):
                 tour[j] = node2
     return tour
     
-def perform_swaps(tour):
+def perform_swaps(tour, distance_matrix):
     '''
     perform swaps on the tour until the objective value does not change
     '''
     # initialize swaps
-    last_objective = evaluate_objective(tour)
-    best_tour = swaps(tour)
-    current_objective = evaluate_objective(tour)
+    last_objective = evaluate_objective(tour, distance_matrix)
+    best_tour = swaps(tour, distance_matrix)
+    current_objective = evaluate_objective(tour, distance_matrix)
     while True:
-        tour = swaps(tour)
-        current_objective = evaluate_objective(tour)
+        tour = swaps(tour, distance_matrix)
+        current_objective = evaluate_objective(tour, distance_matrix)
         if int(current_objective) == int(last_objective):
             best_objective = current_objective
             best_tour = tour[:]
             break
         else:
             last_objective = current_objective
+
+    return best_tour
+
+
+
+def nearest_neighbor(tour, distance_matrix):
+
+    
+
+
 
     return best_tour
