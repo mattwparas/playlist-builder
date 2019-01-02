64e269bda52a56362e54f1cb878382c91e4bf227
frogbird <matthewparas2020@u.northwestern.edu>
2019-01-02 16:48:44 -0600

    Updated example.py

--- setlist/example.py
+++ setlist/example.py
@@ -1,33 +1,44 @@
-SPOTIPY_CLIENT_ID = # YOUR ID HERE
-SPOTIPY_CLIENT_SECRET = # YOUR SECRET HERE
+import os
+from get_data import *
+
+SPOTIPY_CLIENT_ID = os.environ['MY_CLIENT_ID']
+SPOTIPY_CLIENT_SECRET = os.environ['MY_CLIENT_SECRET']
 SPOTIPY_REDIRECT_URI = 'http://localhost:8888/'
 
+# scope = 'playlist-modify-public'
+username = "frog_bird" # your username here
+playlist_name = "Test Playlist 2" # your playlist name here
 scope = 'playlist-read-private'
-username = "frog_bird"
-playlist_name = "Stone's Birthday Bash"
 
 token = util.prompt_for_user_token(username, scope, client_id=SPOTIPY_CLIENT_ID,
 client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
 
-# reset files
-files_to_remove = ["testplaylist.json", "featurelist.json", "tracklist.json"]
+playlistid, spot = get_playlist_id(token, username, playlist_name)
 
-for path in files_to_remove:
-    if os.path.exists(path):
-        os.remove(path)
-    else:
-        print("The file,", path, "does not exist")
+# get tracks from the playlist first
+tracklist = get_playlist_tracks(username, playlistid, spot)
 
-pid, sp = spt.get_playlist_id(token, username, playlist_name)
+# get feature for the playlist
+features = get_features(tracklist, spot)
 
-tracks = spt.get_playlist_tracks(username, pid, sp)
-spt.save_to_json('tracklist.json', tracks)
+# clean the response
+feature_values = [x for x in feature_list if x is not None]
 
-playlist_uris = [item['track']['uri'] for item in tracks]
+# get the length of the playlist
+playist_length = len(feature_values)
 
-feature_list = spt.get_features(tracks, sp)
-spt.save_to_json('featurelist.json', feature_list)
+# generates a cos wave to fit the songs to
+user_function = discrete_cos(playist_length, 4, min_value=0.5)
 
+single_fit = Fit_Regression(feature_values, 'energy', user_function)
+single_fit.perform_swaps()
+single_fit.pprint()
+single_fit.graph_results()
 
-data = spt.get_playlist_audio_analysis(playlist_uris, sp)
-spt.save_to_json('testplaylist.json', data)
+uris = [x['uri'] for x in single_fit.reordered_features]
+
+##### Warning #####
+# This will replace all the meta data for the playlist, as it removes all the songs and re adds them
+# If this is important to you, seek another method
+spot.user_playlist_replace_tracks(username, playlistid, uris)
+
