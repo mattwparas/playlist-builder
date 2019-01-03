4bb6dd947990d2e99e36c80fd120ea2b253cd02d
frogbird <matthewparas2020@u.northwestern.edu>
2019-01-03 16:00:07 -0600

    removed old example files, updated docstrings

--- setlist/save_data.py
+++ setlist/save_data.py
@@ -2,20 +2,25 @@ import json
 def save_to_json(path, data):
     '''
     Wrapper to save data to json
-    inputs:
-        path - exactly what it sounds like
-        data - json data to save
+    Args:
+        path (string): exactly what it sounds like
+        data (json object): json data to save
+    Returns:
+        None
     '''
     js = json.dumps(data)
     fp = open(path, 'a')
     fp.write(js)
     fp.close()
+    return
 
 def open_file(path):
     '''
     Wrapper to open data to json
-    inputs:
-        path - exactly what it sounds like - leads to json file
+    Args:
+        path (string): Path to data to open
+    Returns:
+        The json data
     ''' 
     json1_file = open(path)
     json1_str = json1_file.read()
